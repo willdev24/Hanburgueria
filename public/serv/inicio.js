52949ee998075bac4ad05a386abf6cf80ff3719b
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-06-06 16:57:46 -0400

    videos

---
+++ public/serv/inicio.js
@@ -0,0 +1,36 @@
+let time = 2000
+let currentImagenIndex = 0
+let imagens = document.querySelectorAll("#baner img")
+let max = imagens.length
+
+const satrtF = ()=>{
+
+
+    function nextImage(){
+
+        currentImagenIndex ++
+
+if(currentImagenIndex >= max){
+    currentImagenIndex = 0
+    for(let i = 0; i < 4; i++) 
+    imagens[i].classList.remove("selected")
+
+}
+
+
+imagens[currentImagenIndex].classList.add("selected")
+
+
+    }
+
+setInterval(()=>{
+
+nextImage()
+
+}, time)
+
+
+
+}
+
+window.addEventListener("load", satrtF)
